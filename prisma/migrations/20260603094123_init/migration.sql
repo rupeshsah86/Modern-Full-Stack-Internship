2bcf9eab92a2f1738eb51c46d0599107adf47caa
rupeshsah86 <rupeshkumarsah.2024cse@sece.ac.in>
2026-06-03 16:04:05 +0530

    Day 8 Prisma signup login feature

---
+++ prisma/migrations/20260603094123_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FoodItems" (
+    "id" SERIAL NOT NULL,
+    "item_name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "FoodItems_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
